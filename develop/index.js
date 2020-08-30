3e33243b44c01c71b713fe77c748267151ae2796
marcuspramirez <marcus.p.ramirez2020@gmail.com>
2020-08-30 18:27:29 -0500

    npm installed

---
+++ develop/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+       {
+           type: "input",
+           message: "What is your user name?",
+           name: "username",
+       } 
+    ]);
+
+console.log(prompt)
+
+
+
+    
+
+
