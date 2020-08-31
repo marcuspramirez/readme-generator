4a28bfbb65c88ebe0a0993c41678b190a816e1de
marcuspramirez <marcus.p.ramirez2020@gmail.com>
2020-08-31 10:52:30 -0500

    added questions

--- develop/index.js
+++ develop/index.js
@@ -1,19 +1,44 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
+const fs = require("fs");
+const inquirer = require("inquirer");
+
 
 inquirer
     .prompt([
-       {
-           type: "input",
-           message: "What is your user name?",
-           name: "username",
-       } 
+        {
+            type: "input",
+            message: "What is the title of your project?",
+            name: "title",
+        },
+        {
+            type: "input",
+            message: "What is your Github user name?",
+            name: "username",
+        },
+        {
+            type: "input",
+            message: "Describe your project.",
+            name: "description",
+        },
+        {
+            type: "input",
+            message: "What steps are required to install your project?",
+            name: "install_steps",
+        },
+        {
+            type: "input",
+            message: "Provide instructions and examples for use.",
+            name: "usage",
+        },
+        {
+
+        }
+
+
     ]);
 
-console.log(prompt)
 
 
 
-    
+
 
 
